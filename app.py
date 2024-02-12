f232086fe7f9e1725aacae0dc636b20ba8aac1a1
Fouad El Azbi <elazbi@Fouad-mac.local>
2024-02-12 12:07:06 +0100

    Working on blog

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index_page():
     return render_template('index.html')
 
+@app.route('/myBlog')
+def single_page():
+    return render_template('single.html')
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
